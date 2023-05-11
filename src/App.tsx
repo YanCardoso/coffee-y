6bbc0802921ddf0534013027231c0622952bebd5
YanCardoso <yancard7@gmail.com>
2023-05-11 14:07:02 -0300

    first

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <>
+        <h1>HOME</h1>
+      </>
+    </ThemeProvider>
+  );
+}
+
+export default App;
